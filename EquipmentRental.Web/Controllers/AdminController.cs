3d87db0baff1c109bac61fdd99b980340e5227c7
mmahmood233 <m7mdm233@gmail.com>
2025-04-25 22:35:23 +0300

    added the admin dashboard

---
+++ EquipmentRental.Web/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using EquipmentRental.DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Authorize(Roles = "Administrator")]
+public class AdminController : Controller
+{
+    private readonly EquipmentRentalDbContext _context;
+
+    public AdminController(EquipmentRentalDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Logs()
+    {
+        var logs = _context.Logs
+            .Include(l => l.User)
+            .OrderByDescending(l => l.Timestamp)
+            .ToList();
+
+        return View(logs);
+    }
+
+    public IActionResult ManageUsers()
+    {
+        var users = _context.Users.Include(u => u.Role).ToList();
+        return View(users);
+    }
+
+
+}
