fe7ee924b0cdf3ad73aa1fe0a8262c075ddc1d62
Jackson1x1x <134522053+Jackson1x1x@users.noreply.github.com>
2025-05-03 14:00:38 +0300

    RentalTransaction

---
+++ EquipmentRental.Web/Controllers/RentalTransactionsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EquipmentRental.DataAccess.Models;
+
+namespace EquipmentRental.Web.Controllers
+{
+    public class RentalTransactionsController : Controller
+    {
+        private readonly EquipmentRentalDbContext _context;
+
+        public RentalTransactionsController(EquipmentRentalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RentalTransactions
+        public async Task<IActionResult> Index()
+        {
+            var equipmentRentalDbContext = _context.RentalTransactions.Include(r => r.Customer).Include(r => r.Equipment).Include(r => r.RentalRequest);
+            return View(await equipmentRentalDbContext.ToListAsync());
+        }
+
+        // GET: RentalTransactions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.RentalTransactions == null)
+            {
+                return NotFound();
+            }
+
+            var rentalTransaction = await _context.RentalTransactions
+                .Include(r => r.Customer)
+                .Include(r => r.Equipment)
+                .Include(r => r.RentalRequest)
+                .FirstOrDefaultAsync(m => m.RentalTransactionId == id);
+            if (rentalTransaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(rentalTransaction);
+        }
+
+        // GET: RentalTransactions/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Users, "UserId", "Email");
+            ViewData["EquipmentId"] = new SelectList(_context.Equipment, "EquipmentId", "AvailabilityStatus");
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RentalRequestId", "Status");
+            return View();
+        }
+
+        // POST: RentalTransactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RentalTransactionId,RentalRequestId,EquipmentId,CustomerId,RentalStartDate,RentalEndDate,RentalPeriod,RentalFee,Deposit,PaymentStatus,CreatedAt")] RentalTransaction rentalTransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rentalTransaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Users, "UserId", "Email", rentalTransaction.CustomerId);
+            ViewData["EquipmentId"] = new SelectList(_context.Equipment, "EquipmentId", "AvailabilityStatus", rentalTransaction.EquipmentId);
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RentalRequestId", "Status", rentalTransaction.RentalRequestId);
+            return View(rentalTransaction);
+        }
+
+        // GET: RentalTransactions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.RentalTransactions == null)
+            {
+                return NotFound();
+            }
+
+            var rentalTransaction = await _context.RentalTransactions.FindAsync(id);
+            if (rentalTransaction == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Users, "UserId", "Email", rentalTransaction.CustomerId);
+            ViewData["EquipmentId"] = new SelectList(_context.Equipment, "EquipmentId", "AvailabilityStatus", rentalTransaction.EquipmentId);
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RentalRequestId", "Status", rentalTransaction.RentalRequestId);
+            return View(rentalTransaction);
+        }
+
+        // POST: RentalTransactions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RentalTransactionId,RentalRequestId,EquipmentId,CustomerId,RentalStartDate,RentalEndDate,RentalPeriod,RentalFee,Deposit,PaymentStatus,CreatedAt")] RentalTransaction rentalTransaction)
+        {
+            if (id != rentalTransaction.RentalTransactionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rentalTransaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentalTransactionExists(rentalTransaction.RentalTransactionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Users, "UserId", "Email", rentalTransaction.CustomerId);
+            ViewData["EquipmentId"] = new SelectList(_context.Equipment, "EquipmentId", "AvailabilityStatus", rentalTransaction.EquipmentId);
+            ViewData["RentalRequestId"] = new SelectList(_context.RentalRequests, "RentalRequestId", "Status", rentalTransaction.RentalRequestId);
+            return View(rentalTransaction);
+        }
+
+        // GET: RentalTransactions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.RentalTransactions == null)
+            {
+                return NotFound();
+            }
+
+            var rentalTransaction = await _context.RentalTransactions
+                .Include(r => r.Customer)
+                .Include(r => r.Equipment)
+                .Include(r => r.RentalRequest)
+                .FirstOrDefaultAsync(m => m.RentalTransactionId == id);
+            if (rentalTransaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(rentalTransaction);
+        }
+
+        // POST: RentalTransactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.RentalTransactions == null)
+            {
+                return Problem("Entity set 'EquipmentRentalDbContext.RentalTransactions'  is null.");
+            }
+            var rentalTransaction = await _context.RentalTransactions.FindAsync(id);
+            if (rentalTransaction != null)
+            {
+                _context.RentalTransactions.Remove(rentalTransaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalTransactionExists(int id)
+        {
+          return (_context.RentalTransactions?.Any(e => e.RentalTransactionId == id)).GetValueOrDefault();
+        }
+    }
+}
