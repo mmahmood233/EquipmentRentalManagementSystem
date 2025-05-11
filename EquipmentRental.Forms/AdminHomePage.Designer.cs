790d4b472cf53180b5497f5056d5bdf64fa7c2c7
Shad-en <abdullahazhar20202@gmail.com>
2025-05-12 00:25:21 +0300

    changes

--- EquipmentRental.Forms/AdminHomePage.Designer.cs
+++ EquipmentRental.Forms/AdminHomePage.Designer.cs
@@ -31,13 +31,17 @@
             button1 = new Button();
             label1 = new Label();
             button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(234, 222);
+            button1.Location = new Point(126, 104);
+            button1.Margin = new Padding(2, 1, 2, 1);
             button1.Name = "button1";
-            button1.Size = new Size(291, 46);
+            button1.Size = new Size(157, 22);
             button1.TabIndex = 3;
             button1.Text = "Rental Requests";
             button1.UseVisualStyleBackColor = true;
@@ -47,30 +51,67 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 36F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(84, 46);
+            label1.Location = new Point(45, 22);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(597, 128);
+            label1.Size = new Size(299, 65);
             label1.TabIndex = 2;
             label1.Text = "Home Page";
             // 
             // button2
             // 
-            button2.Location = new Point(234, 303);
+            button2.Location = new Point(126, 142);
+            button2.Margin = new Padding(2, 1, 2, 1);
             button2.Name = "button2";
-            button2.Size = new Size(291, 46);
+            button2.Size = new Size(157, 22);
             button2.TabIndex = 4;
             button2.Text = "Rental Transactions";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // button3
+            // 
+            button3.Location = new Point(126, 184);
+            button3.Margin = new Padding(2, 1, 2, 1);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 22);
+            button3.TabIndex = 5;
+            button3.Text = "Dashboard";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(126, 229);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Equipment";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(126, 271);
+            button5.Name = "button5";
+            button5.Size = new Size(157, 23);
+            button5.TabIndex = 7;
+            button5.Text = "Logs";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
             // AdminHomePage
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 653);
+            ClientSize = new Size(431, 306);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "AdminHomePage";
             Text = "AdminHomePage";
             ResumeLayout(false);
@@ -82,5 +123,8 @@
         private Button button1;
         private Label label1;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- EquipmentRental.Forms/AdminHomePage.cs
+++ EquipmentRental.Forms/AdminHomePage.cs
@@ -22,14 +22,32 @@ namespace EquipmentRental.Forms
         {
             RentalRequests rentalRequests = new RentalRequests();
             rentalRequests.Show();
-          
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             RentalTransactionsAdmin rentalTransactionsAdmin = new RentalTransactionsAdmin();
             rentalTransactionsAdmin.Show();
-            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DashboardForm dashboardForm = new DashboardForm();
+            dashboardForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EquipmentForm equipmentForm = new EquipmentForm();
+            equipmentForm.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            LogsForm logsForm = new LogsForm();
+            logsForm.Show();
         }
     }
 }
